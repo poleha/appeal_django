49381af8fbdbed427fbc873e802baa416e0a977e
poleha <pass1191>
2016-06-16 16:39:53 +0300

    staticfiles enabled for admin

--- appeal/settings.py
+++ appeal/settings.py
@@ -148,9 +148,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 #MEDIA_URL = '/media/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
