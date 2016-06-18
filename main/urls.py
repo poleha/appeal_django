34a2ef0015eb8199316164104f778574d898a344
poleha <pass1191>
2016-06-18 15:19:29 +0300

    Endpoint for social login fixed

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^posts/(?P<pk>[0-9]+)/rate/$', views.RatePostView.as_view()),
 
     url(r'^user_profile/$', views.UserProfileDetail.as_view()),
-    url(r'^social_login/$', views.SocialLogin.as_view()),
+    url(r'^auth/social_login/$', views.SocialLogin.as_view()),
 
     #url(r'^comments/(?P<pk>[0-9]+)/$', views.CommentDetail.as_view(), name='comment-detail'),
 #    url(r'^ajax_login/$', views.LoginView.as_view()),
